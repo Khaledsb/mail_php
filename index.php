e995847f98751ae1145886df08cb4ef14a0a454d
khaled sebti <sebti.khaled1@gmail.com>
2016-05-29 12:15:02 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 
+     $to      = 'shipudennaruto699@gmail.com';
+     $subject = 'le sujet';
+     $message = 'Bonjour !';
+     $headers = 'From: shipudennaruto699@gmail.com' . "\r\n" .
+     'Reply-To: shipudennaruto699@gmail.com' . "\r\n" .
+     'X-Mailer: PHP/' . phpversion();
 
-echo "hello from php";
+     mail($to, $subject, $message, $headers);
 ?>
